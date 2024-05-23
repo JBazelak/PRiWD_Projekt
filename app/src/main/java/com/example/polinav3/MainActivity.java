1c79427ad880ca57ce7c0043e8aeb4100bc3519c
Degern1024 <102228685+Degern1024@users.noreply.github.com>
2024-05-23 08:41:06 +0200

    Controller
    Basic controller functions

--- app/src/main/java/com/example/polinav3/MainActivity.java
+++ app/src/main/java/com/example/polinav3/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.polinav3;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.InputDevice;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
 
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -9,6 +13,10 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.polinav3.gamepad.GameControllerService;
 import com.sanbot.opensdk.base.TopBaseActivity;
+import com.sanbot.opensdk.beans.FuncConstant;
+import com.sanbot.opensdk.function.unit.SpeechManager;
+
+import java.util.ArrayList;
 
 public class MainActivity extends TopBaseActivity {
 
@@ -23,10 +31,155 @@ public class MainActivity extends TopBaseActivity {
             return insets;
         });
 
+        ArrayList<Integer> list = getGameControllerIds();
+        Log.d("controllers_amount", String.valueOf(list.size()));
+//        TextView v = findViewById(R.id.textStart);
+//        v.setText("controllers:"+String.valueOf(list.size()));
+
         startService(new Intent(this, GameControllerService.class));
     }
 
     @Override
     protected void onMainServiceConnected() {
+        SpeechManager sm = (SpeechManager) getUnitManager(FuncConstant.SPEECH_MANAGER);
+        if (sm != null) {
+//            sm.startSpeak("Dzień dobry dzieciątka!");
+        }
+        ArrayList<Integer> list = getGameControllerIds();
+        Log.d("controllers_amount", String.valueOf(list.size()));
     }
+
+    private void JoystickAction(float x, float y) {
+//        TextView v = findViewById(R.id.textStart);
+//        v.setText("X:"+String.valueOf(x)+"|Y:"+String.valueOf(y));
+    }
+    @Override
+    public  boolean dispatchKeyEvent(KeyEvent event) {
+        Log.d("test", "button");
+//        TextView v = findViewById(R.id.textStart);
+//        v.setText("buutton:"+String.valueOf(event.getKeyCode()));
+        switch(event.getKeyCode()) {
+            case KeyEvent.KEYCODE_BUTTON_L1:
+            case KeyEvent.KEYCODE_BUTTON_R1:
+            case KeyEvent.KEYCODE_BUTTON_THUMBR:
+            case KeyEvent.KEYCODE_BUTTON_THUMBL:
+            case KeyEvent.KEYCODE_DPAD_LEFT:
+            case KeyEvent.KEYCODE_DPAD_RIGHT:
+            case KeyEvent.KEYCODE_DPAD_UP:
+            case KeyEvent.KEYCODE_DPAD_DOWN:
+            case KeyEvent.KEYCODE_BUTTON_START:
+            case KeyEvent.KEYCODE_BUTTON_MODE://Big button in the middle
+            case KeyEvent.KEYCODE_BUTTON_B:
+            case KeyEvent.KEYCODE_BUTTON_A:
+            case KeyEvent.KEYCODE_BUTTON_X:
+            case KeyEvent.KEYCODE_BUTTON_Y:
+                Log.d("przycisk", String.valueOf(event.getKeyCode()));
+            default:
+                return super.onKeyDown(event.getKeyCode(), event);
+        }
+    }
+
+    @Override
+    public boolean onGenericMotionEvent(MotionEvent event) {
+
+        // Check that the event came from a game controller
+        if ((event.getSource() & InputDevice.SOURCE_JOYSTICK) ==
+                InputDevice.SOURCE_JOYSTICK &&
+                event.getAction() == MotionEvent.ACTION_MOVE) {
+
+            // Process all historical movement samples in the batch
+            final int historySize = event.getHistorySize();
+
+            // Process the movements starting from the
+            // earliest historical position in the batch
+            for (int i = 0; i < historySize; i++) {
+                // Process the event at historical position i
+                processJoystickInput(event, i);
+            }
+
+            // Process the current movement sample in the batch (position -1)
+            processJoystickInput(event, -1);
+            return true;
+        }
+        return super.onGenericMotionEvent(event);
+    }
+    private void processJoystickInput(MotionEvent event,
+                                      int historyPos) {
+
+        InputDevice inputDevice = event.getDevice();
+
+        // Calculate the horizontal distance to move by
+        // using the input value from one of these physical controls:
+        // the left control stick, hat axis, or the right control stick.
+        float x = getCenteredAxis(event, inputDevice,
+                MotionEvent.AXIS_X, historyPos);
+        if (x == 0) {
+            x = getCenteredAxis(event, inputDevice,
+                    MotionEvent.AXIS_HAT_X, historyPos);
+        }
+        if (x == 0) {
+            x = getCenteredAxis(event, inputDevice,
+                    MotionEvent.AXIS_Z, historyPos);
+        }
+
+        // Calculate the vertical distance to move by
+        // using the input value from one of these physical controls:
+        // the left control stick, hat switch, or the right control stick.
+        float y = getCenteredAxis(event, inputDevice,
+                MotionEvent.AXIS_Y, historyPos);
+        if (y == 0) {
+            y = getCenteredAxis(event, inputDevice,
+                    MotionEvent.AXIS_HAT_Y, historyPos);
+        }
+        if (y == 0) {
+            y = getCenteredAxis(event, inputDevice,
+                    MotionEvent.AXIS_RZ, historyPos);
+        }
+        JoystickAction(x,y);
+        // Update the ship object based on the new x and y values
+    }
+
+    private static float getCenteredAxis(MotionEvent event,
+                                         InputDevice device, int axis, int historyPos) {
+        final InputDevice.MotionRange range =
+                device.getMotionRange(axis, event.getSource());
+
+        // A joystick at rest does not always report an absolute position of
+        // (0,0). Use the getFlat() method to determine the range of values
+        // bounding the joystick axis center.
+        if (range != null) {
+            final float flat = range.getFlat();
+            final float value =
+                    historyPos < 0 ? event.getAxisValue(axis):
+                            event.getHistoricalAxisValue(axis, historyPos);
+
+            // Ignore axis values that are within the 'flat' region of the
+            // joystick axis center.
+            if (Math.abs(value) > flat) {
+                return value;
+            }
+        }
+        return 0;
+    }
+
+    public ArrayList<Integer> getGameControllerIds() {
+        ArrayList<Integer> gameControllerDeviceIds = new ArrayList<Integer>();
+        int[] deviceIds = InputDevice.getDeviceIds();
+        for (int deviceId : deviceIds) {
+            InputDevice dev = InputDevice.getDevice(deviceId);
+            int sources = dev.getSources();
+
+            // Verify that the device has gamepad buttons, control sticks, or both.
+            if (((sources & InputDevice.SOURCE_GAMEPAD) == InputDevice.SOURCE_GAMEPAD)
+                    || ((sources & InputDevice.SOURCE_JOYSTICK)
+                    == InputDevice.SOURCE_JOYSTICK)) {
+                // This device is a game controller. Store its device ID.
+                if (!gameControllerDeviceIds.contains(deviceId)) {
+                    gameControllerDeviceIds.add(deviceId);
+                }
+            }
+        }
+        return gameControllerDeviceIds;
+    }
+
 }
