80971fcce3e5d7de8269342ab5edd0c2b50b57e4
Nico <nicokun1316@gmail.com>
2024-05-23 08:00:19 +0200

    rename button enum

--- app/src/main/java/com/example/polinav3/ButtonInput.java
+++ app/src/main/java/com/example/polinav3/ButtonInput.java
@@ -1,13 +1,13 @@
 package com.example.polinav3;
 
 public class ButtonInput {
-    public ButtonInput(Buttons button, float axis, boolean pressed) {
+    public ButtonInput(ButtonType button, float axis, boolean pressed) {
         this.button = button;
         this.axis = axis;
         this.pressed = pressed;
     }
 
-    public final Buttons button;
+    public final ButtonType button;
     public final float axis;
     public final boolean pressed;
 }
