95e4722356269cd1582daac0789a1b740bb045e5
Oskar Desecki <27292@s.am.szczecin.pl>
2024-06-19 14:41:04 +0200

    Add UI and fix bugs

--- app/src/main/java/com/example/polinav3/MainActivity.java
+++ app/src/main/java/com/example/polinav3/MainActivity.java
@@ -2,6 +2,12 @@ package com.example.polinav3;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Switch;
+import android.widget.TextView;
 
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -11,22 +17,40 @@ import com.example.polinav3.gamepad.GameControllerService;
 import com.sanbot.opensdk.base.TopBaseActivity;
 
 public class MainActivity extends TopBaseActivity {
+    private Button buttonPodazanie;
+    private Button buttonRozmowa;
+    private Button buttonRozpoznawanie;
+    private ImageButton buttonBack;
+    private Switch switchProjektor;
+    private Switch switchSwiatlo;
+    private Switch switchAnimacjaZycia;
+    private Switch switchPad;
+    private Switch switchPolaczniePC;
+    private TextView textViewBatteryStatus;
+    private ProgressBar progressBarBattery;
+    private ImageView imageViewLogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        register(MainActivity.class);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
-        startService(new Intent(this, GameControllerService.class));
+        buttonPodazanie = findViewById(R.id.buttonPodazanie);
+        buttonRozmowa = findViewById(R.id.buttonRozmowa);
+        buttonRozpoznawanie = findViewById(R.id.buttonRozpoznawanie);
+        buttonBack = findViewById(R.id.buttonBack);
+        switchProjektor = findViewById(R.id.switchProjektor);
+        switchSwiatlo = findViewById(R.id.switchSwiatlo);
+        switchAnimacjaZycia = findViewById(R.id.switchAnimacjaZycia);
+        switchPad = findViewById(R.id.switchPad);
+        switchPolaczniePC = findViewById(R.id.switchPolaczniePC);
+        textViewBatteryStatus = findViewById(R.id.textViewBatteryStatus);
+        progressBarBattery = findViewById(R.id.progressBarBattery);
+        imageViewLogo = findViewById(R.id.imageViewLogo);
     }
 
     @Override
     protected void onMainServiceConnected() {
+
     }
 }
