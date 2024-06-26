9445f70c79993d758709eb95e14ff75864d857b5
nico <nicokun1316@gmail.com>
2024-06-26 23:54:53 +0200

    make her move

--- app/src/main/java/com/example/polinav3/gamepad/GameControllerService.java
+++ app/src/main/java/com/example/polinav3/gamepad/GameControllerService.java
@@ -3,9 +3,12 @@ package com.example.polinav3.gamepad;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
 
 import com.sanbot.opensdk.base.BindBaseService;
 import com.sanbot.opensdk.beans.FuncConstant;
+import com.sanbot.opensdk.function.beans.wheelmotion.NoAngleWheelMotion;
+import com.sanbot.opensdk.function.beans.wheelmotion.RelativeAngleWheelMotion;
 import com.sanbot.opensdk.function.unit.HandMotionManager;
 import com.sanbot.opensdk.function.unit.WheelMotionManager;
 import com.sanbot.opensdk.function.unit.WingMotionManager;
@@ -13,9 +16,11 @@ import com.sanbot.opensdk.function.unit.WingMotionManager;
 public class GameControllerService extends BindBaseService {
     private WheelMotionManager wheels;
     private WingMotionManager wings;
+    private int x, y;
     public GameControllerService() {
-
+        Log.d("test", "creating service");
     }
+    public static GameControllerService INSTANCE = null;
 
 
     @Override
@@ -26,13 +31,60 @@ public class GameControllerService extends BindBaseService {
     }
 
     @Override
+    public void onCreate() {
+        super.onCreate();
+        register(GameControllerService.class);
+    }
+
+    @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         return START_STICKY;
     }
 
     @Override
     protected void onMainServiceConnected() {
+        Log.d("test", "connecting service");
         wheels = (WheelMotionManager) getUnitManager(FuncConstant.WHEELMOTION_MANAGER);
         wings = (WingMotionManager) getUnitManager(FuncConstant.WINGMOTION_MANAGER);
+        INSTANCE = this;
+
+        new Thread(this::roboLoop).start();
+    }
+
+    private void roboLoop() {
+        if (this.y != 0 || this.x != 0) {
+            byte action = xyToAction(x, y);
+            wheels.doNoAngleMotion(new NoAngleWheelMotion(action, 3, 125));
+        }
+
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private byte xyToAction(float x, float y) {
+        if (y > 0) {
+            if (x > 0) {
+                return NoAngleWheelMotion.ACTION_RIGHT_FORWARD;
+            } else if (x < 0) {
+                return NoAngleWheelMotion.ACTION_LEFT_FORWARD;
+            }
+            return NoAngleWheelMotion.ACTION_FORWARD;
+        } else if (y < 0) {
+            if (x > 0) {
+                return NoAngleWheelMotion.ACTION_RIGHT_BACK;
+            } else if (x < 0) {
+                return NoAngleWheelMotion.ACTION_LEFT_BACK;
+            }
+            return NoAngleWheelMotion.ACTION_BACK;
+        }
+        return NoAngleWheelMotion.ACTION_STOP;
+    }
+
+    public void move(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 }
