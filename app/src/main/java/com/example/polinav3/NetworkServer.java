cec520bcf3fb850a1d76e173be068c7a70c4ccb4
Degern1024 <102228685+Degern1024@users.noreply.github.com>
2024-07-01 12:14:13 +0200

    temporary commit
    temporary commit to save progress

---
+++ app/src/main/java/com/example/polinav3/NetworkServer.java
@@ -0,0 +1,53 @@
+package com.example.polinav3;
+
+import android.util.Log;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+public class NetworkServer extends Thread {
+    private ServerSocket serverSocket;
+    private int port = 30000;
+
+
+    private Socket clientSocket;
+    private BufferedReader br;
+
+    //przechowuje i emituje Socket'y
+//    private List<Thread> controllers;
+
+    public void run(){
+//        controllers = new ArrayList<Thread>();
+        try {
+            serverSocket = new ServerSocket(port);
+            clientSocket = serverSocket.accept();
+            br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            String out;
+            out = br.readLine();
+            while((out = br.readLine())!=null){
+                Log.d("nep", out);
+            }
+
+        } catch(Exception e) {
+            System.out.println(e);
+        }
+        Log.d("nep", "test");
+    }
+    public void shutdown(){
+        try {
+            br.close();
+            clientSocket.close();
+            serverSocket.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+
+}
