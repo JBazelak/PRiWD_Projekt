c862c399f3a6e9ee69a5f8303bca6deb7e383f83
nico <nicokun1316@gmail.com>
2024-07-01 21:14:28 +0200

    make her move

--- app/src/main/java/com/example/polinav3/gamepad/GameControllerService.java
+++ app/src/main/java/com/example/polinav3/gamepad/GameControllerService.java
@@ -17,6 +17,7 @@ public class GameControllerService extends BindBaseService {
     private WheelMotionManager wheels;
     private WingMotionManager wings;
     private float x, y;
+    private boolean running = true;
     public GameControllerService() {
         Log.d("test", "creating service");
     }
@@ -52,33 +53,53 @@ public class GameControllerService extends BindBaseService {
     }
 
     private void roboLoop() {
-        if (this.y != 0 || this.x != 0) {
-            byte action = xyToAction(x, y);
-            wheels.doNoAngleMotion(new NoAngleWheelMotion(action, 3, 125));
-        }
+        while (running) {
+            if (this.y != 0 || this.x != 0) {
+                byte action = xyToAction(x, y);
+                Log.d("action", String.valueOf(action));
+                wheels.doNoAngleMotion(new NoAngleWheelMotion(action, 3, 125));
+            }
 
-        try {
-            Thread.sleep(100);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
     private byte xyToAction(float x, float y) {
-        if (y > 0) {
-            if (x > 0) {
+        Log.d("move", "x: " + x + "; y: " + y);
+        if (y > 0.4) {
+            if (x > 0.4) {
+                Log.d("move", "right forward");
                 return NoAngleWheelMotion.ACTION_RIGHT_FORWARD;
-            } else if (x < 0) {
+            } else if (x < -0.4) {
+
+                Log.d("move", "left forward");
+
                 return NoAngleWheelMotion.ACTION_LEFT_FORWARD;
             }
+            Log.d("move", " forward");
+
             return NoAngleWheelMotion.ACTION_FORWARD;
-        } else if (y < 0) {
-            if (x > 0) {
+        } else if (y < -0.4) {
+            if (x > 0.4) {
+                Log.d("move", "right back");
+
                 return NoAngleWheelMotion.ACTION_RIGHT_BACK;
-            } else if (x < 0) {
+            } else if (x < -0.4) {
+                Log.d("move", "left back");
+
                 return NoAngleWheelMotion.ACTION_LEFT_BACK;
             }
+            Log.d("move", " back");
+
             return NoAngleWheelMotion.ACTION_BACK;
+        } else if (x > 0.4) {
+            return NoAngleWheelMotion.ACTION_RIGHT;
+        } else if (x < -0.4) {
+            return NoAngleWheelMotion.ACTION_LEFT;
         }
         return NoAngleWheelMotion.ACTION_STOP;
     }
@@ -86,5 +107,9 @@ public class GameControllerService extends BindBaseService {
     public void move(float x, float y) {
         this.x = x;
         this.y = y;
+    }
+
+    public void stop() {
+        wheels.doNoAngleMotion(new NoAngleWheelMotion(NoAngleWheelMotion.ACTION_STOP, 0));
     }
 }
