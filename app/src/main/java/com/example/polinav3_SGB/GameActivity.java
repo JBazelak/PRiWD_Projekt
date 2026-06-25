5735bfec050c0e10d3277b6b02f7033c37a35516
Bartosz Glanowski <bartosz.glanowski@outlook.com>
2026-06-25 13:48:30 +0200

    Even more UI changes

--- app/src/main/java/com/example/polinav3_SGB/GameActivity.java
+++ app/src/main/java/com/example/polinav3_SGB/GameActivity.java
@@ -13,12 +13,14 @@ import com.sanbot.opensdk.beans.OperationResult;
 import com.sanbot.opensdk.function.beans.EmotionsType;
 import com.sanbot.opensdk.function.beans.FaceRecognizeBean;
 import com.sanbot.opensdk.function.beans.LED;
+import com.sanbot.opensdk.function.beans.wing.AbsoluteAngleWingMotion;
 import com.sanbot.opensdk.function.unit.HDCameraManager;
 import com.sanbot.opensdk.function.beans.StreamOption;
 import com.sanbot.opensdk.function.unit.HardWareManager;
 import com.sanbot.opensdk.function.unit.MediaManager;
 import com.sanbot.opensdk.function.unit.SpeechManager;
 import com.sanbot.opensdk.function.unit.SystemManager;
+import com.sanbot.opensdk.function.unit.WingMotionManager;
 import com.sanbot.opensdk.function.unit.interfaces.media.FaceRecognizeListener;
 
 import java.io.ByteArrayOutputStream;
@@ -37,6 +39,8 @@ public class GameActivity extends TopBaseActivity{
     private SystemManager systemManager;
     private HardWareManager hardWareManager;
     private SpeechManager speechManager;
+    private WingMotionManager wingMotionManager;
+    private AbsoluteAngleWingMotion AbsoluteAngleWingMotion;
     private int streamHandle = -1;
     ByteArrayOutputStream stream = new ByteArrayOutputStream();
     StringBuilder stringBuilder = new StringBuilder();
@@ -54,21 +58,25 @@ public class GameActivity extends TopBaseActivity{
             public void onClick(View view) {
                 textView.setText("Zagrajmy!");
                 speechManager.startSpeak("Zagrajmy!");
+                AbsoluteAngleWingMotion absoluteAngleWingMotion = new AbsoluteAngleWingMotion(AbsoluteAngleWingMotion.PART_RIGHT,5,80);
                 executor.execute(new Runnable() {
                     @Override
                     public void run() {
-<<<<<<< Updated upstream
                         // petla while(true)
                         // robot mowi "Marynarzyk!" czeka chwile
-=======
+                        wingMotionManager.doAbsoluteAngleMotion(absoluteAngleWingMotion);
                         images.clear();
-
->>>>>>> Stashed changes
                         for (int i = 0; i<4; i++){
                             try {
                                 images.add(captureSingleFrame());
                                 textView.setText(speechBubbles[i]);
                                 speechManager.startSpeak(speechBubbles[i]);
+                                wingMotionManager.doAbsoluteAngleMotion(absoluteAngleWingMotion);
+                                if (i%2 == 0){
+                                    wingMotionManager.doAbsoluteAngleMotion(new AbsoluteAngleWingMotion(AbsoluteAngleWingMotion.PART_RIGHT,5,90));
+                                } else {
+                                    wingMotionManager.doAbsoluteAngleMotion(new AbsoluteAngleWingMotion(AbsoluteAngleWingMotion.PART_RIGHT,5,80));
+                                }
                                 Thread.sleep(250);
                             } catch (InterruptedException e) {
                                 throw new RuntimeException(e);
@@ -104,7 +112,9 @@ public class GameActivity extends TopBaseActivity{
         hdCameraManager = (HDCameraManager) getUnitManager(FuncConstant.HDCAMERA_MANAGER);
         systemManager = (SystemManager) getUnitManager(FuncConstant.SYSTEM_MANAGER);
         hardWareManager = (HardWareManager) getUnitManager(FuncConstant.HARDWARE_MANAGER);
-        speechManager = (SpeechManager) getUnitManager(FuncConstant.SPEECH_MANAGER);        StreamOption streamOption = new StreamOption();
+        speechManager = (SpeechManager) getUnitManager(FuncConstant.SPEECH_MANAGER);
+        wingMotionManager = (WingMotionManager) getUnitManager(FuncConstant.WINGMOTION_MANAGER);
+        StreamOption streamOption = new StreamOption();
         streamOption.setChannel(StreamOption.MAIN_STREAM); // Główny strumień HD (1280*720) [cite: 1073]
         streamOption.setDecodType(StreamOption.HARDWARE_DECODE); // Użycie dekodowania sprzętowego [cite: 1068]
         streamOption.setJustIframe(false); // Pozwala pobierać wszystkie klatki, a nie tylko klatki kluczowe I-frame [cite: 1070]
@@ -138,13 +148,9 @@ public class GameActivity extends TopBaseActivity{
     }
 
     public void getRobotReaction(String result){
-<<<<<<< Updated upstream
-        // pozmieniac parametry swiatel, teraz zbyt szybko mrugaja
-=======
         byte slowDown = (byte) 6;
         byte randomCount = (byte) 0;
 
->>>>>>> Stashed changes
         if (result.equals("robot_wins")){
             systemManager.showEmotion(EmotionsType.SNICKER);
             hardWareManager.setLED(new LED(LED.PART_LEFT_HEAD, LED.MODE_FLICKER_GREEN, slowDown, randomCount));
@@ -182,6 +188,7 @@ public class GameActivity extends TopBaseActivity{
         if (hdCameraManager != null && streamHandle != -1) {
             hdCameraManager.closeStream(streamHandle);
         }
+        wingMotionManager.doAbsoluteAngleMotion(new AbsoluteAngleWingMotion(AbsoluteAngleWingMotion.PART_RIGHT,5,0));
         executor.shutdown();
     }
 
